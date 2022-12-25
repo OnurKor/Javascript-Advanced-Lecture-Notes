508a81ba1247587cf795c1c91e1725bf898d4e20
Onur <onurkordogan@gmail.com>
2022-12-26 02:34:40 +0300

    taksit

--- 02_Dom-Events-Examples/kredi-hesaplama/app.js
+++ 02_Dom-Events-Examples/kredi-hesaplama/app.js
@@ -1,6 +1,30 @@
-console.log('**** Kredi Hesaplama *******');
+console.log("**** Kredi Hesaplama *******");
 
+const select = document.querySelector(".form-select");
+const vade = document.querySelector("#vade");
+const tutar = document.querySelector("#tutar");
+const hesaplaBtn = document.querySelector(".btn-dark");
+let oran = 0;
+let taksit = 0;
 
-
+hesaplaBtn.addEventListener("click", (e) => {
   //? preventDefault() event'ın default davranışı (submit etmek ve formu silmek) engeller
- 
+  e.preventDefault();
+  if (select.value === "Konut Kredisi") {
+    oran = 1.29;
+  } else if (select.value === "Ihtiyac Kredisi") {
+    oran = 1.99;
+  } else if (select.value === "Arac Kredisi") {
+    oran = 1.79;
+  }
+  // console.log(oran);
+  if (!select.value || !vade.value || !tutar.value) {
+    alert("Lütfen Kredi türü, Vade ve tutarı giriniz");
+  }
+
+  const faiz = oran / 100;
+  taksit =
+    (tutar.value * (faiz * (1 + faiz) ** vade.value)) /
+    ((1 + faiz) ** vade.value - 1);
+    // console.log(taksit);
+});
