7eb837878b615135ab0295c0489245a320a0e202
Onur <onurkordogan@gmail.com>
2022-12-26 22:00:41 +0300

    local-and-session-storage

---
+++ 03_Checkout_Page_Solution/checkout.js
@@ -0,0 +1,12 @@
+const taxRate = 0.18;
+const shippingPrice = 15.0;
+
+window.addEventListener("load", () => {
+  //?set item to LocalStorage
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  
+  //?set item to SessionStorage
+  sessionStorage.setItem("taxRate", taxRate);
+  sessionStorage.setItem("shippingPrice", shippingPrice);
+});
