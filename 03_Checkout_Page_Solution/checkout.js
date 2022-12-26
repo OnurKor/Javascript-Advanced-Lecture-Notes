1dd8c47cfeae596a9d71a1ab95e90c2bf872d5a7
Onur <onurkordogan@gmail.com>
2022-12-27 00:07:09 +0300

    break

--- 03_Checkout_Page_Solution/checkout.js
+++ 03_Checkout_Page_Solution/checkout.js
@@ -21,18 +21,20 @@ productsDiv.addEventListener("click", (event) => {
     let quantityP = event.target.nextElementSibling;
     if (quantityP.innerText > 1) {
       quantityP.innerText--;
-      calculateProductAndCartTotal();
-    }else{
+      //*parameter == selected productInfoDiv
+      calculateProductAndCartTotal(event.target.parentElement.parentElement);
+    } else {
       if (confirm("Product will be deleted")) {
         event.target.parentElement.parentElement.parentElement.remove();
         calculateCartTotal();
       }
-    }    
+    }
     // console.log(typeof event.target.nextElementSibling.innerText);
     // console.log("minus button clicked");
   } else if (event.target.classList.contains("plus")) {
     event.target.previousElementSibling.innerText++;
-    calculateProductAndCartTotal();
+    //*parameter == selected productInfoDiv
+    calculateProductAndCartTotal(event.target.parentElement.parentElement);
     // console.log(event.target.classList);
     // console.log("plus button clicked");
   } else if (event.target.classList.contains("remove-product")) {
@@ -41,7 +43,19 @@ productsDiv.addEventListener("click", (event) => {
     // console.log("remove button clicked");
   } else {
     console.log("other elements clicked");
-  };
+  }
   // console.log(event.target);
 });
 
+//? calculate cart and product totals
+const calculateProductAndCartTotal = (productInfoDiv) => {
+  //! product calculation
+  let quantity = 
+  let price = 
+
+  //! cart calculation
+  calculateCartTotal();
+};
+
+//? calculate cart totals
+const calculateCartTotal = () => {};
