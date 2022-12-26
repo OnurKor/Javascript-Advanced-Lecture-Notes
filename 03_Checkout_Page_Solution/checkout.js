9b35eeb960497aa373c1cd985213f3a9c6f84d70
Onur <onurkordogan@gmail.com>
2022-12-26 23:09:06 +0300

    capturingMethods

--- 03_Checkout_Page_Solution/checkout.js
+++ 03_Checkout_Page_Solution/checkout.js
@@ -2,11 +2,30 @@ const taxRate = 0.18;
 const shippingPrice = 15.0;
 
 window.addEventListener("load", () => {
-  //?set item to LocalStorage
+  //?set item to LocalStorage => sürekli kalır biz silene kadar
   localStorage.setItem("taxRate", taxRate);
   localStorage.setItem("shippingPrice", shippingPrice);
-  
-  //?set item to SessionStorage
-  sessionStorage.setItem("taxRate", taxRate);
-  sessionStorage.setItem("shippingPrice", shippingPrice);
+
+  //?set item to SessionStorage => sayfayı kapattıgımızda silinir.
+  /*  sessionStorage.setItem("taxRate", taxRate);
+  sessionStorage.setItem("shippingPrice", shippingPrice); */
+}); //! web database'imiz diyebiliriz.
+
+//? capturing method
+
+let productsDiv = document.querySelector(".products");
+// console.log(productsDiv);
+
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "minus") {
+    console.log("minus button clicked");
+  } else if (event.target.classList.contains("plus")) {
+    // console.log(event.target.classList);
+    console.log("plus button clicked");
+  } else if (event.target.classList.contains("remove-product")) {
+    console.log("remove button clicked");
+  } else {
+    console.log("other elements clicked");
+  };
+  // console.log(event.target);
 });
