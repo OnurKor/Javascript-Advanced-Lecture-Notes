136c447493a0a3817a9730547bdc21f773dd0f8b
Onur <onurkordogan@gmail.com>
2022-12-30 18:12:13 +0300

    async-timeout

---
+++ 04_Async1/1-timeout_Interval.js
@@ -0,0 +1,51 @@
+//*=================================================================
+//*              1- Senkron-Asenkron Programlama
+//* ================================================================
+
+//! Javascript, bir single-threaded ve Asenkron Programlama dilidir.
+
+//? Asenkron Programlama
+//? --------------------------------------------------------------
+//? Asenkron Programlama, bir alt gorevin uygulamanin asil thread'inden
+//? bagimsiz olarak arka planda calistirilmasina izin veren paralel programlama
+//? teknigidir. Bu alt gorev tamamlandiginda (basriyla veya basarisizlikla)
+//? asil thread bu konuda bilgilendirilir. Asenkron programlama, uygulamalarin
+//? performansininin artirilmasina ve daha iyi kullanici deneyimine katki saglamaktadir.
+
+//? Ozellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri,
+//? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Asyn Programlama
+//? kullanilmasi cok onemlidir.
+
+//* Senkron
+//* ---------------------------------------------------------------
+
+
+
+//* Asenkron (setTimeout)
+//* ---------------------------------------------------------------
+
+
+
+//* Asenkron (setInterval, clearInterval)
+//*----------------------------------------------------------
+
+
+
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakta bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+
+
+
+//? COZUMLER:
+//?----------------------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
+
