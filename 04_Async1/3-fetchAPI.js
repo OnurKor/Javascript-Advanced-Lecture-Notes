b2b75c08f7a202bf9cba40f8e9f6f286582cb2fa
Onur <onurkordogan@gmail.com>
2023-01-16 23:39:37 +0300

    comment

--- 04_Async1/3-fetchAPI.js
+++ 04_Async1/3-fetchAPI.js
@@ -21,17 +21,17 @@
 //   .then((data) => updateDom(data))
 //   .catch((err) => console.log(err));
 
-const updateDom = (data) => {
-  const userDiv = document.querySelector(".users");
+// const updateDom = (data) => {
+//   const userDiv = document.querySelector(".users");
 
-  data.forEach((user) => {
-    //! destructuring
-    const { login, avatar_url, html_url } = user;
-    userDiv.innerHTML += `
-        <h2 class="text-primary"><span class="text-black">NAME: </span>${login}</h2>
-        <img src=${avatar_url} width="50%" alt=""/>
-        <h3>HTML_URL: ${html_url}</h3>
-        <h4>LOGIN: ${login}</h4>
-        `;
-  });
-};
+//   data.forEach((user) => {
+//     //! destructuring
+//     const { login, avatar_url, html_url } = user;
+//     userDiv.innerHTML += `
+//         <h2 class="text-primary"><span class="text-black">NAME: </span>${login}</h2>
+//         <img src=${avatar_url} width="50%" alt=""/>
+//         <h3>HTML_URL: ${html_url}</h3>
+//         <h4>LOGIN: ${login}</h4>
+//         `;
+//   });
+// };
