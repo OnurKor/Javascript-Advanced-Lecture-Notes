928b1fed7bf6ffa7a687ff2bcac0b4ab0177e6f5
Onur <onurkordogan@gmail.com>
2023-01-22 23:13:20 +0300

    update

--- 05_Async2/async-await/4-async-await.js
+++ 05_Async2/async-await/4-async-await.js
@@ -34,17 +34,6 @@ const getUsers = async function () {
 
 getUsers();
 
-fetch("https://api.github.com/users")
-  .then((res) => {
-    //! error handling
-    if (!res.ok) {
-      throw new Error(`Something went wrong: ${res.status}`);
-    }
-    return res.json();
-  })
-  .then((data) => updateDom(data))
-  .catch((err) => console.log(err));
-
 const updateDom = (data) => {
   const userDiv = document.querySelector(".users");
 
